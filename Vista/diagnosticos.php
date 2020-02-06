dd1a1ff5ac6eeaa853a2b308009596b4c2178393
mojitoaCh861 <andych04@gmail.com>
2020-02-06 18:41:48 -0300

    pag eliminar

--- Vista/diagnosticos.php
+++ Vista/diagnosticos.php
@@ -64,32 +64,10 @@ $Coleccion = new ColeccionDiagnostico();
                                                             </button>
                                                         </a>
 
-                                                        <a title="Eliminar" href="#">
-                                                            <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModal">
+                                                        <a title="Eliminar" href="diagnostico.eliminar.php">
+                                                            <button type="button" class="btn btn-outline-danger">
                                                                 <span class="oi oi-trash"></span>
-                                                            </button>
-                                                            
-                                                            <!-- Modal -->
-                                                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                                <div class="modal-dialog" role="document">
-                                                                    <div class="modal-content">
-                                                                        <div class="modal-header">
-                                                                            <h5 class="modal-title" id="exampleModalLabel">Eliminar Diagnostico</h5>
-                                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                                <span aria-hidden="true">&times;</span>
-                                                                            </button>
-                                                                        </div>
-                                                                        <div class="modal-body">
-                                                                           Segurito? 
-                                                                        </div>
-                                                                        <div class="modal-footer">
-                                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                                                                            <button type="button" class="btn btn-outline-danger" href="#">Eliminar</button>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-
+                                                            </button>                                   
                                                         </a>
                                                     </td>
                                                 </tr>
