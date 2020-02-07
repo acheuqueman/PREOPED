aab96b3bb6574db1e12f62fcc5bc812fa37634f3
mojitoaCh861 <andych04@gmail.com>
2020-02-06 21:21:35 -0300

    comentario

---
+++ Vista/diagnostico.eliminar.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * diagnostico: texto
+ * tipo_discapacidad: selectoptions o radio
+ * descripcion: textarea
+ * 
+ */
+include_once '../lib/Constantes.Class.php';
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../lib/bootstrap-4.1.1-dist/css/bootstrap.css" />
+        <link rel="stylesheet" href="../lib/open-iconic-master/font/css/open-iconic-bootstrap.css" />
+        <script type="text/javascript" src="../lib/JQuery/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="../lib/bootstrap-4.1.1-dist/js/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="../mod_autocompletar/jquery-3.1.1.min.js"></script>
+        <link rel="stylesheet" href="../mod_autocompletar/jquery-ui.min.css" />
+        <script type="text/javascript" src="../mod_autocompletar/jquery-ui.min.js"></script>
+        <title><?= Constantes::NOMBRE_SISTEMA; ?> - Inicio</title>
+    </head>
+    <body>
+        <?php include_once '../gui/navbar.php'; ?>
+
+        <div class="container-fluid">
+            
+
+                <div class="row">
+                    <div class="col-md-12 justify-content-center">
+
+                        <div class="row ml-auto">
+                            <!-- LUGAR DE LA LISTA -->
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="oi oi-trash">Eliminar Diagnosticos</h5>
+                                    </div>
+                                    <form action="diagnostico.eliminar.procesar.php" method="POST">
+                                        <div class="row">&nbsp;</div>
+                                        <div class="row">
+                                            
+                                            <!-- traer name, tipo, descripccion-->
+                                            
+                                            
+                                        </div>
+                                        <div class="row">&nbsp;</div>
+                                        <div class="row justify-content-end">
+                                             <div class="col-1">
+                                                 <button class="btn btn-light">Cancelar</button>
+                                            </div>
+                                            <div class="col-2">
+                                                 <input type ="submit" class="btn btn-info" value="Eliminar Diagnostico">  
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="row">&nbsp;</div>
+                                    </form>
+                                </div>   
+                            </div> 
+
+                        </div>
+
+                        <div class="row">&nbsp;</div>
+
+                    </div>
+                </div>
+
+            
+        </div>
+
+        <?php include_once '../gui/footer.php'; ?>
+    </body>
+</html>
+
+
+
+
+
+
