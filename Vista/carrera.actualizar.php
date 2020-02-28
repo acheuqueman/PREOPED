f20abbbd5dc8791d5fc2bcb2b9357519e5516fea
FacundoBracamonte <facundobracamonte@protonmail.com>
2020-02-28 16:02:55 -0300

    carrera.form

--- Vista/carrera.actualizar.php
+++ Vista/carrera.actualizar.php
@@ -3,16 +3,14 @@ include_once '../lib/Constantes.Class.php';
 include_once '../modelo/Carrera.class.php';
 include_once '../modelo/CarreraMapper.php';
 
-$Carrera = new Carrera($_POST);
 $Mapper = new CarreraMapper();
-$idCarreraCreado = $Mapper->update($Carrera);
+$Asignatura = new Carrera($Mapper->findById($_GET['id']));
 ?>
-
 <html>
     <head>
         <?php include_once '../lib/includesCss.php'; ?>
         <?php include_once '../lib/includesJs.php'; ?>
-        <title><?= Constantes::NOMBRE_SISTEMA; ?> - Carreras</title>
+        <title><?= Constantes::NOMBRE_SISTEMA; ?> - Inicio</title>
     </head>
     <body>
         <?php include_once '../gui/navbar.php'; ?>
@@ -20,29 +18,19 @@ $idCarreraCreado = $Mapper->update($Carrera);
         <div class="container-fluid">
             <div class="card">
                 <div class="card-header">
-                    <h5 class="oi oi-plus"> Actualizar Asignatura</h5>
+                    <h5 class="oi oi-plus">Actualizar Carrera</h5>
                 </div>
                 <div class="card-body">
-                    <?php if ($idAsignaturaCreado) { ?>
-                        <p class="alert alert-success">Operaci&oacute;n realizada con &eacute;xito.</p>
-                        <p>Asignatura cargado correctamente.</p>
-                    <?php } ?>
-                    <?php if (!$idAsignaturaCreado) { ?>
-                        <p class="alert alert-danger">Hubo un error</p>
-                        <p>No fue posible cargar la Asignatura. Por favor, intente nuevamente. Si el problema persiste, contacte el administrador del sistema.</p>
-                    <?php } ?>                                        
-                </div>
-                <div class="card-footer">
-                    <p>Opciones:</p>
-                    <p>
-                        <a href="Carreras.php"><input class="btn btn-outline-danger" value="Salir" /></a>
-                    </p>
+                    <form action="carrera.actualizar.procesar.php" method="POST">
+                        <div class="card-body">
+                            <?php include_once './carrera.formulario.php'; ?>
+                        </div>
+                    </form>
                 </div>
             </div>   
-            <div class="row">&nbsp;</div>
-        </div>
 
+        </div>
 
         <?php include_once '../gui/footer.php'; ?>
     </body>
-</html>  
+</html>
